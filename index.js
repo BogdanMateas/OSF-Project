ea4be7779f2100e2c1059556d31c5b54d2e3abac
BogdanMateas <bogdani.mateas@yahoo.com>
2020-03-10 00:55:17 +0200

    static carousel and json added

--- index.js
+++ index.js
@@ -203,3 +203,11 @@ $(document).ready(() => {
     }
   });
 });
+
+fetch("./data.json")
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
